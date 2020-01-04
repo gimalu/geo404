63959003a190aa09a249d64c7580b5d32e41c779
gimalu <gina.maria.lukaszczyk@googlemail.com>
2020-01-04 22:14:24 +0100

    feat(main): Changed bounding box due to memory allocation problems

--- Luk_Gina_geo404.R
+++ Luk_Gina_geo404.R
@@ -1,5 +1,5 @@
 # GEO 404 - Assignment 1
-# Gina Luk
+# Gina Lukaszczyk
 
 # ------------------------------------------------------------------------------
 # Environmental Settings
@@ -9,11 +9,17 @@ library("sf")
 library("stringr")
 library("rgeos")
 library("rgdal")
-library("ggplot2")
 library("leaflet")
+library("tspmeta")
+library("osrm")
+library("cartography")
+library("rgrass7")
+library("osmdata")
+
 
 # URI ------------------------------------------------------------------
 URI.plz = "https://www.suche-postleitzahl.org/download_files/public/plz-gebiete.shp.zip"
+URI.grass = "C:/Program Files/GRASS GIS 7.8"
 
 # Destination Paths ----------------------------------------------------
 dest.plz = tempfile("plz-gebiete", fileext = c(".zip"))  # Create a temporary file
@@ -35,6 +41,7 @@ aug.shp = plz.shp[which((plz.shp$plz >= 86150 & plz.shp$plz <= 86199)), ]
 aug.shp = as(aug.shp, "Spatial")
 
 aug.center = gCentroid(aug.shp)  # Get center point
+
 aug.map = leaflet() %>% setView(lng = aug.center$x, lat = aug.center$y, zoom = 11)
 
 # Plot Postal Codes ----------------------------------------------------
@@ -44,6 +51,8 @@ aug.map %>% addTiles() %>% addPolygons(data=aug.shp, weight=1, col = 'red',
                                  highlightOptions = highlightOptions(color = "white", 
                                                                      weight = 2,
                                                                      bringToFront = TRUE))
+rm(plz.shp)
+rm(aug.shp)
 
 # ------------------------------------------------------------------------------
 # DM Location
@@ -53,15 +62,16 @@ url = paste0(
   "https://www.dm.de/cms/restws/stores/find?requestingCountry=DE&", 
   "countryCodes=DE%2CAT%2CBA%2CBG%2CSK%2CRS%2CHR%2CCZ%2CRO%2CSI%", 
   "2CHU%2CMK&mandantId=100", 
-  "&bounds=", aug.center$y - 0.5, 
-  "%2C", aug.center$x - 0.5,
-  "%7C", aug.center$y + 0.5, 
-  "%2C", aug.center$x + 0.5, 
+  "&bounds=", aug.center$y - 0.05, 
+  "%2C", aug.center$x - 0.05,
+  "%7C", aug.center$y + 0.05,
+  "%2C", aug.center$x + 0.05, 
   "&before=false&after=false&morningHour=9&eveningHour=18&_", 
   "=1479236790492")
 
 dm.json = jsonlite::fromJSON(url)
 saveRDS(dm.json, dest.dm)
+
 dm.json = readRDS(dest.dm)
 
 # Cleaning Data -------------------------------------------------------
@@ -85,6 +95,16 @@ dm.data = data.frame(city=dm.json$address$city, street=temp.address,
                      y=as.numeric(temp.y),
                      parking=dm.json$parkingTooltip)
 
+dm.sf = st_as_sf(x=dm.data, 
+                 coords=c("x", "y"),
+                 crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+
+dm.sf = as(dm.sf, "Spatial")
+
+rm(i)
+rm(temp.address)
+rm(temp.x)
+rm(temp.y)
 
 # Plot DM Shops around Augsburg ---------------------------------------
 dm.icon = makeIcon("./data/icon/dm_icon_20.png", iconWidth=20, iconHeight=20)
@@ -99,5 +119,3 @@ dm.map  =  leaflet(dm.data) %>%
              icon=dm.icon[1])%>% 
   addLegend("bottomright", colors= dm.icon[1], labels="DM'", title="DM Shops um Augsburg")
 dm.map
-
-
